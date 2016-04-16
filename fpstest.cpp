e2bb4caa8e81e4f3184472c23ace1b6244a021a8
Nick <ngardner1@csub.edu>
2016-04-15 20:59:03 -0700

    stuff

--- fpstest.cpp
+++ fpstest.cpp
@@ -74,7 +74,11 @@ struct Vec {
 
 
 struct Game {
-    Vec pos;    
+    Vec pos;
+    int moveX;
+    int moveY;
+    float velocityX;
+    float velocityY;
 };
 
 //Function prototypes
@@ -104,6 +108,10 @@ int main(void)
     game.pos.x = 0.0;
     game.pos.y = 0.0;
     game.pos.z = 0.0;
+    game.moveX = 0;
+    game.moveY = 0;
+    game. velocityX = 0;
+    game.velocityY = 0;
     
     //start animation
     while(!done) {
@@ -237,12 +245,12 @@ void init_opengl(void)
     float w = WINDOW_WIDTH;
     float h = WINDOW_HEIGHT;
     //OpenGL initialization
-    //glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     //Initialize matrices
     glMatrixMode(GL_PROJECTION); glLoadIdentity();
     glMatrixMode(GL_MODELVIEW); glLoadIdentity();
     
-    glFrustum(-1.0/h*w, 1.0/h*w, -1, 1, 1.5, 200.0);
+    glFrustum(-0.1/h*w, 0.1/h*w, -0.1, 0.1, 0.15, 200.0);
     //Set the screen background color
     glClearColor(1.0, 1.0, 1.0, 1.0);
     ///////////////////////////////////
@@ -250,17 +258,17 @@ void init_opengl(void)
     initialize_fonts();
 
 
-   //glEnable(GL_COLOR_MATERIAL);
+   glEnable(GL_COLOR_MATERIAL);
    
-   //glEnable(GL_LIGHTING);
-   //glEnable(GL_LIGHT0);
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
    ShowCursor(0);
 
     
-   GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-   GLfloat mat_shininess[] = { 50.0 };
-   GLfloat light_position[] = { 0.0, 0.0, 0.0, 0.0 };
+   GLfloat mat_specular[] = { 0.1, 0.1, 0.1, 1.0 };
+   GLfloat mat_shininess[] = { 0.0 };
+   GLfloat light_position[] = { 0.0, 1.0, 0.0, 0.0 };
    glClearColor (0.0, 0.0, 0.0, 0.0);
    glShadeModel (GL_SMOOTH);
 
@@ -299,10 +307,10 @@ void check_mouse(XEvent *e, Game *game)
     rotx = fmod(rotx,2.0*PI);
     if (rotx < 0.0)
 	rotx += PI * 2.0;
-    if (roty > PI)
-	roty = PI;
-    if (roty < 0)
-	roty = 0;
+    if (roty > PI / 2.0)
+	roty = PI / 2.0;
+    if (roty < -PI / 2.0)
+	roty = -PI / 2.0;
     if (movex != 0 || movey != 0)
         XWarpPointer(dpy,root,root,0,0,WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_WIDTH/2,WINDOW_HEIGHT/2);
     
@@ -318,21 +326,57 @@ int check_keys(XEvent *e, Game *game)
             return 1;
         }
 	if (key == XK_w) {
-	    float dirz = cos(rotx);
-	    float dirx = sin(rotx);
-	    game->pos.z += dirz;
-	    game->pos.x += dirx;
-	    cout << game->pos.x << " " << game->pos.y << " " << game->pos.z << "\n";
+        game->moveX = 1;
 	}
+	if (key == XK_s) {
+        game->moveX = -1;
+    }
+    if (key == XK_a) {
+        game->moveY = -1;
+    }
+    if (key == XK_d) {
+        game->moveY = 1;
+    }
         //You may check other keys here.
         
+    }else if (e->type == KeyRelease) {
+        int key = XLookupKeysym(&e->xkey, 0);
+        if (key == XK_w) {
+            game->moveX = 0;
+        }
+        if (key == XK_s) {
+            game->moveX = 0;
+        }
+        if (key == XK_a) {
+            game->moveY = 0;
+        }
+        if (key == XK_d) {
+            game->moveY = 0;
+        }        
     }
     return 0;
+
 }
 
 void movement(Game *game)
 {
-    
+    if (game->moveY == 0 || game->moveX == 0){
+        //moving orthogonally
+        game->velocityX = (float)game->moveX;
+        game->velocityY = (float)game->moveY;
+    } else {
+        //moving diagonally
+        game->velocityX = (float)game->moveX / 1.414;
+        game->velocityY = (float)game->moveY / 1.414;
+    }
+    /*
+        float dirz = cos(rotx);
+        float dirx = sin(rotx);
+        game->pos.z += dirz;
+        game->pos.x -= dirx;
+        */
+    game->pos.z += (game->velocityX * cos(rotx) + game->velocityY * -sin(rotx)) / 5.0;
+    game->pos.x -= (game->velocityY * cos(rotx) + game->velocityX * sin(rotx)) / 5.0;
 }
 
 Vec normal(Vec A, Vec B, Vec C)
@@ -366,18 +410,88 @@ Vec normalize(Vec A)
     return A;
 }
 
+void HUD(Game *game)
+{
+    int w = WINDOW_WIDTH;
+    int h = WINDOW_HEIGHT;
+    glDepthMask(GL_FALSE);
+    glDisable(GL_DEPTH_TEST);  
+    glDisable(GL_LIGHTING);
+    
+    
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    glOrtho(0.0, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0, -1.0, 10.0);
+    glMatrixMode(GL_MODELVIEW);
+    //glPushMatrix();        ----Not sure if I need this
+    glLoadIdentity();
+    glDisable(GL_CULL_FACE);
+    
+    glClear(GL_DEPTH_BUFFER_BIT);
+    int l = h / 1;
+    
+    glBegin(GL_LINES);
+    glColor3f(1.0f,1.0f,1.0f);
+    glVertex2d(w / 2 - l / 20, h / 2);
+    glVertex2d(w / 2 + l / 20, h / 2);
+    glEnd();
+    glBegin(GL_LINES);
+    glVertex2d(w / 2, h / 2 - l / 20);
+    glVertex2d(w / 2, h / 2 + l / 20);
+    glEnd();
+    
+    glBegin(GL_LINES);
+    glColor3f(0.0f,0.0f,0.0f);
+    glVertex2d(w / 2 - l / 20, h / 2+1);
+    glVertex2d(w / 2 + l / 20, h / 2+1);
+    glEnd();
+    glBegin(GL_LINES);
+    glVertex2d(w / 2+1, h / 2 - l / 20);
+    glVertex2d(w / 2+1, h / 2 + l / 20);
+    glEnd();
+    
+    // Making sure we can render 3d again
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    
+    glDepthMask(GL_TRUE);
+    glEnable(GL_DEPTH_TEST);  
+    glEnable(GL_LIGHTING);
+    glFrustum(-0.1/WINDOW_HEIGHT*WINDOW_WIDTH, 0.1/WINDOW_HEIGHT*WINDOW_WIDTH, -0.1, 0.1, 0.15, 200.0);
+}
+
 
 void render(Game *game)
 {
-    static float val = 0.0;
-    val += 0.01;
-    float ra = 5.0;
+
+    float ra = 20.0;
     //Rect r;
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    float top = 2.0 * (cos(val) + 1.0) + 1.0;
+    float top = 4.0;
     float bot = -2.0;
     
     Vec v[6];
+    Vec c[6];
+    c[0].x = 0.5;
+    c[0].y = 0.5;
+    c[0].z = 1.0;
+    c[1].x = 1.0;
+    c[1].y = 1.0;
+    c[1].z = 1.0;
+    c[2].x = 1.0;
+    c[2].y = 0.0;
+    c[2].z = 0.0;
+    c[3].x = 0.0;
+    c[3].y = 0.0;
+    c[3].z = 1.0;
+    c[4].x = 0.0;
+    c[4].y = 1.0;
+    c[4].z = 0.0;
+    c[5].x = 1.0;
+    c[5].y = 1.0;
+    c[5].z = 0.0;
     v[0].x = v[3].x = v[7].x = v[4].x = ra;
     v[1].x = v[2].x = v[5].x = v[6].x = -ra;
     
@@ -386,29 +500,33 @@ void render(Game *game)
     
     v[0].z = v[1].z = v[5].z = v[4].z = -ra;
     v[2].z = v[3].z = v[6].z = v[7].z = ra;
-    /*int ctop[] = {4,5,6,7};
-    int cbot[] = {0,1,2,3};
-    int cfront[] = {0,1,5,4};
-    int cback[] = {3,2,6,7};
-    int cleft[] = {1,2,6,5};
-    int cright[] = {0,3,7,4};
-    int *verts[] = {ctop,cbot,cfront,cback,cleft,cright};*/
-    int verts[][4] = {{4,5,6,7},{0,1,2,3},{0,1,5,4},{3,2,6,7},{1,2,6,5},{0,3,7,4}};
+
+    int verts[][4] = {{4,5,6,7},{3,2,1,0},{0,1,5,4},{7,6,2,3},{1,2,6,5},{4,7,3,0}};
     glPushMatrix();
-        glRotatef(roty / PI * 180.0 - 90.0,1,0,0);
-        glRotatef(rotx / PI * 180.0,0,1,0);
+    
+    glRotatef(roty / PI * 180.0,1,0,0);
+    glRotatef(rotx / PI * 180.0,0,1,0);
+    glTranslatef(game->pos.x, game->pos.y, game->pos.z);
+    
     for (int i = 0; i < 6; i++) {
-        glColor3ub(255 / 6 * i,0,255 - 255 / 6 * i);
-        
+        glColor3f(c[i].x,c[i].y,c[i].z);
         glBegin(GL_POLYGON);
-        //Vec N = normal(a,b,c);
-        //glNormal3f(N.x,N.y,N.z);
+        Vec N = normal(v[verts[i][0]],v[verts[i][1]],v[verts[i][2]]);
+        glNormal3f(N.x,N.y,N.z);
         for (int j = 0; j < 4; j++){
             glVertex3f(v[verts[i][j]].x, v[verts[i][j]].y, v[verts[i][j]].z);
         }
         glEnd();
-        
+        glDisable(GL_LIGHTING);
+        glBegin(GL_LINE_STRIP);
+        glColor3f(1.0f,1.0f,1.0f);
+        glVertex3f((v[verts[i][0]].x+v[verts[i][2]].x)/2.0 + N.x, (v[verts[i][0]].y+v[verts[i][2]].y)/2.0 + N.y, (v[verts[i][0]].z+v[verts[i][2]].z)/2.0 + N.z);
+        glVertex3f((v[verts[i][0]].x+v[verts[i][2]].x)/2.0, (v[verts[i][0]].y+v[verts[i][2]].y)/2.0, (v[verts[i][0]].z+v[verts[i][2]].z)/2.0);
+        glEnd();
+        glEnable(GL_LIGHTING);
     }
+    
+    
     glPopMatrix();
     
   /*
@@ -434,7 +552,7 @@ void render(Game *game)
     sprintf(buff,"%f",roty);
     ggprint16(&r, 16, 0x00222222, buff);
     */
-    
+    HUD(game);
     glFlush ();
     
 }
